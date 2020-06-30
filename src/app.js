e405bcd3288ce03be88d66089df13bd569ebd2e3
Collins Orlando <collinstheuncoder@gmail.com>
2020-06-30 22:51:47 +0100

    set up cors

--- src/app.js
+++ src/app.js
@@ -10,30 +10,11 @@ import './db';
 
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://rgb-challenge-1988.netlify.app',
-];
-
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
-app.use(
-  cors({
-    origin(origin, cb) {
-      if (!origin) return cb(null, true);
-
-      if (!allowedOrigins.includes(origin)) {
-        const msg =
-          'The CORS policy for this site does not allow access from the specified Origin.';
-
-        return cb(new Error(msg), false);
-      }
-      return cb(null, true);
-    },
-  })
-);
+app.use(cors());
 
 // Routes Config
 app.use('/auth', authRouter);
